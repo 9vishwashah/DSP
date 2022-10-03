f7d476f299888075802de97d6059b6a435360ed0
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:21:01 +0530

    Create largest_list.py

---
+++ largest_list.py
@@ -0,0 +1,9 @@
+# Write a Python program to get the largest number from a list.
+l=[34, 56 , 78, 86]
+m= l[ 0 ]
+for a in l:
+    if a > m:
+            m = a
+print("max num in list is",m)
+# OUTPUT
+# max num in list is 86
