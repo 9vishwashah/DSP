6c0882b1b82a6f24c6e52bfd2948fcd636d4cdd6
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:37:12 +0530

    Create area_peri_square.py

---
+++ area_peri_square.py
@@ -0,0 +1,10 @@
+#5.Write a program to calculate area and perimeter of the square
+s=int(input("Enter Side : "))
+area=s*s
+perimeter=4*s
+print(f"{area} is Area of Rectangle ")
+print(f"{perimeter} is Perimeter of Rectangle")
+# OUTPUT
+# Enter Side : 5
+# 25 is Area of Rectangle
+# 20 is Perimeter of Rectangle
