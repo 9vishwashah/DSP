3bb2f9159d5d227dfb373afd52ab42c57ea67c2f
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:16:01 +0530

    Create sum_natural.py

---
+++ sum_natural.py
@@ -0,0 +1,9 @@
+# 3.Write a Python program to find the sum of first 10 natural numbers using for loop.
+n=10
+sum1 = 0
+while(n > 0):
+    sum1=sum1+n
+    n=n-1
+print(f"{sum1} is The sum of first 10 natural numbers")
+# OUTPUT
+# 55 is The sum of first 10 natural numbers
