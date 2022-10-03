47e8b71ca129a6cb5a39ff7fc3329ca0fb80fedb
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:36:37 +0530

    Create area_rect.py

---
+++ area_rect.py
@@ -0,0 +1,9 @@
+ # 4.Write a program to find the area of Rectangle
+l = float(input('Enter the length of a Rectangle: '))
+b = float(input('Enter the breadth of a Rectangle: '))
+Area = l * b
+print(f"{Area} is Area of a Rectangle")
+# OUTPUT
+# Enter the length of a Rectangle: 5
+# Enter the breadth of a Rectangle: 2
+# 10.0 is Area of a Rectangle
