85e3db468b22a3954b8a8e6a64809e6a499a419e
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:19:26 +0530

    Create positive_negative.py

---
+++ positive_negative.py
@@ -0,0 +1,11 @@
+# 13.Write a program to check if a Number is Positive, Negative or Zero
+n=int(input("Enter a number "))
+if n>0:
+    print("the number is positive")
+elif n<0:
+   print("the number is negative")
+else:
+    print("the number is zero") 
+#OUTPUT
+# Enter a number 15
+# the number is positive
