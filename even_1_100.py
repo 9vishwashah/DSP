39091e8ba78073143f364a5bc832a5eb9705eb25
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:15:39 +0530

    Create even_1_100.py

---
+++ even_1_100.py
@@ -0,0 +1,11 @@
+# 2.Write a Python program to print all even numbers between 1 to 100 using while loop.
+num = 2
+while num <= 100:
+    print(num)
+    num = num + 2
+# OUTPUT
+# 2
+# 4
+# 6
+# 8
+# 10 .... 100
