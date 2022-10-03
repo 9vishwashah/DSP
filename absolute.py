8c2416f338274e056000d4fcef945bb84cf09f5c
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:18:28 +0530

    Create absolute.py

---
+++ absolute.py
@@ -0,0 +1,9 @@
+# 10.Write a program to find out absolute value of an input number
+a=int(input("Enter the number"))
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+# OUTPUT
+# Enter the number-9
+# 9
