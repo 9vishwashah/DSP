b9cba0f36d499a617ecdfed4838951f7c36b9db3
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-10-03 18:17:57 +0530

    Create odd_even.py

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+# 9.Write a program to check whether a number is even or odd
+n = int(input("Enter the number: "))
+if n%2==0:
+    print("f{n} is a even number")
+else:
+    print(f"{n} is a odd number")
+# OUTPUT
+# Enter the number: 23
+# 23 is a odd number
